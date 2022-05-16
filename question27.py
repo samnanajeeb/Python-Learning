61fb5a31a876918fc57b77318326024ad3b8d0bc
Samna Najeeb <samna.najeeb@kyndryl.com>
2022-05-16 14:59:00 +0530

    question27

---
+++ question27.py
@@ -0,0 +1,12 @@
+"""27. Write a Python program to concatenate all elements in a list 
+into a string and return it."""
+
+def concatenate_string(list):
+        result = ''
+        for i in list:
+                result += str(i)
+        print(result)
+
+
+if __name__ == '__main__':
+        concatenate_string(['s', 'a', 'm', 'n', 'a'])
